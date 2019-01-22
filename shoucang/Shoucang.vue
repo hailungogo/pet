2b68b6a6d041e10c76777e96babce4ae50fb5488
hailungogo <44639953+hailungogo@users.noreply.github.com>
2019-01-22 14:10:45 +0800

    Add files via upload

---
+++ shoucang/Shoucang.vue
@@ -0,0 +1,44 @@
+<template>
+    <div class="app-shoucang">
+        <!--顶部导航-->
+        <mt-header title="我的收藏" style="background:#00ffff;">
+            <router-link to="/Home" slot="left">
+            <mt-button icon="back"></mt-button>
+            </router-link>
+        </mt-header>
+        <!--宠物表-->
+        <ul class="mui-table-view mui-table-view-chevron">
+				<li class="mui-table-view-cell mui-media" v-for="item in list" :key="item.did">
+					<router-link class="mui-navigate-right" :to="'/Petinfo?did='+item.did">
+						<img class="mui-media-object mui-pull-left" :src="item.img_url">
+						<div class="mui-media-body">
+							{{item.cname}}
+						</div> 
+					</router-link>
+				</li>
+        </ul>
+    </div>
+</template>
+<script>
+export default {
+    data(){
+        return{
+            list:[]
+        }
+    },
+    methods: {
+        getShoucang(){
+               this.$http.get("getShoucang").then(result=>{
+                   this.list=result.body.data;
+                   //console.log(result.body);
+               })
+           }
+    },
+    created(){
+        this.getShoucang()
+    }
+}
+</script>
+<style>
+    
+</style>
